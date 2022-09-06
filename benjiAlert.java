92b5d91ec04a7df8a3c06f82ff44d41cc3d58c17
ninjadude752 <spencerblackwell02@gmail.com>
2022-09-06 12:15:50 -0500

    added image added label and button, set button to close.

--- benjiAlert.java
+++ benjiAlert.java
@@ -1,17 +1,35 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class benjiAlert {
+public class benjiAlert extends Canvas{
 
     public static void main(String[] args){
+
+        Toolkit tooltest = Toolkit.getDefaultToolkit();
+        Image benji = tooltest.getImage("IMG_2765.JPG");
+
+        ImageIcon imgBen = new ImageIcon(benji);
         JFrame test = new JFrame("Benji Alert");
 
-        JButton buttonTest = new JButton("OK");
-        buttonTest.setBounds(100,100,10,10);
+        //Create the image label, and set the location.
+        JLabel testLbl = new JLabel(imgBen);
+        testLbl.setHorizontalAlignment(JLabel.CENTER);
+        testLbl.setVerticalAlignment(JLabel.TOP);
 
-        test.add(buttonTest);
+        //Create okButton, and set the bounds of the button, as well as the action.
+        JButton okButton = new JButton("Ok");
+        okButton.setHorizontalAlignment(JLabel.CENTER);
+        okButton.setBounds(143,100,101,40);
+        okButton.addActionListener( e-> {
+            test.dispose();
+        });
 
-        test.setSize(300,300);
-        test.setLayout(null);
+
+        test.add(okButton);
+        test.add(testLbl);
+
+        test.setSize(400,200);
+        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         test.setVisible(true);
 
     }
